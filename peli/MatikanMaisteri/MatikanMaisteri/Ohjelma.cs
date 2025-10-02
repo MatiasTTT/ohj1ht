ad31046d01db29363a59712055de6339309e5893
Matias <mturpeinen323@gmail.com>
2025-10-02 23:33:33 +0300

    Pohja pelille luotu

---
+++ peli/MatikanMaisteri/MatikanMaisteri/Ohjelma.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace MatikanMaisteri
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using var game = new MatikanMaisteri();
+            game.Run();
+        }
+    }
+}
